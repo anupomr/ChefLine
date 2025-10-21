b870103e03956aaf28e1e0db662be57ffd2b0b2a
anupomr <anupomr@yahoo.com>
2025-10-20 22:16:21 -0400

    Abstract class defined

--- main.py
+++ main.py
@@ -3,6 +3,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from abc import ABC, abstractmethod
+
+
+class Person(ABC):
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+    @abstractmethod
+    def get_role(self):
+        pass
+
+    @abstractmethod
+    def person_activity(self):
+        pass
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
