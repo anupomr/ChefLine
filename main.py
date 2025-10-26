2d0d0282bad9c8ed76b492eb315e95c089d3cb9e
anupomr <anupomr@yahoo.com>
2025-10-25 20:40:57 -0400

    Redis Database connection initiated

--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from abc import ABC, abstractmethod
+import redis
 
+
+# Abstract class
 
 class Person(ABC):
     def __init__(self, user_name, password):
@@ -15,18 +18,53 @@ class Person(ABC):
     def get_role(self):
         pass
 
-    @abstractmethod
-    def person_activity(self):
-        pass
+
+def print_customer():
+    print("Customer")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Customer(Person):
+
+    def __init__(self, user_name, password, address):
+        super().__init__(user_name, password)
+        self.address = address
+
+    def get_role(self):
+        return "Customer"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Cook(Person):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, user_name, password, address):
+        super().__init__(user_name, password)
+        self.address = address
+
+    def get_role(self):
+        return "Cook"
+
+
+class Admin(Person):
+
+    def __init__(self, user_name, password, address):
+        super().__init__(user_name, password)
+        self.address = address
+
+    def get_role(self):
+        return "Admin"
+
+
+r = redis.Redis(
+    host='redis-14057.c52.us-east-1-4.ec2.redns.redis-cloud.com',
+    port=14057,
+    password='87g6Iqv4U73wSpFVlaAak3acu92uaLp3',
+    decode_responses=True
+)
+print(" Customer data Entered into Database")
+cust = Customer("adric@abc.com", "phys67", "81 Bay St")
+role = cust.get_role()
+print(role)
+r.hset(cust.user_name, mapping={
+    "Password": cust.password,
+    "Address": cust.address,
+    "Role": cust.get_role()})
+
